c23c9762273002beb1d59fba466556e918fd4d2b
danicobryan <danicobryan@gmail.com>
2018-03-04 18:33:57 -0800

    worked on home page

--- js/main.js
+++ js/main.js
@@ -39,4 +39,18 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
+
+  let originalBackgroundColor = "black";
+  let originalOpacity = "0";
+  $(".image-link").hover(function(){
+    originalOpacity = $(".image-link > .overlay").css("background-color");
+    originalOpacity = $(".image-link > .overlay").css("opacity");
+    $(this).children(".overlay").css("background-color", "pink").css("opacity", "0.8");
+    $(this).click(function() {
+      $(this).children(".overlay").css("background-color", originalBackgroundColor).css("opacity", originalOpacity);
+    });
+    }, function(){
+    $(this).children(".overlay").css("background-color", originalBackgroundColor).css("opacity", originalOpacity);
+  });
+
 })(jQuery); // End of use strict
